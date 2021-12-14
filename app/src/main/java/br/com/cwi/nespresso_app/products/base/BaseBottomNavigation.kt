aeaa9406c129014c0d91e167207d068b1a53352e
Matheus Souza <matheus.souza@cwi.com.br>
2021-12-14 19:30:50 -0300

    initial commit

---
+++ app/src/main/java/br/com/cwi/nespresso_app/products/base/BaseBottomNavigation.kt
@@ -0,0 +1,8 @@
+package br.com.cwi.nespresso_app.products.base
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseBottomNavigation : AppCompatActivity() {
+
+}
---
+++ app/src/main/java/br/com/cwi/nespresso_app/products/machine/MachineActivity.kt
@@ -0,0 +1,12 @@
+package br.com.cwi.nespresso_app.products.machine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.cwi.nespresso_app.R
+
+class MachineActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_machine)
+    }
+}
