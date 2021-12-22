4023141a71791be724c84ff4d83dcb031d7b067e
Matheus Souza <matheus.souza@cwi.com.br>
2021-12-21 22:55:24 -0300

    refact

--- app/src/main/java/br/com/cwi/nespresso_app/presentation/feature/products/coffeeInfo/DetailedCoffeeActivity.kt
+++ app/src/main/java/br/com/cwi/nespresso_app/presentation/feature/products/coffeeInfo/DetailedCoffeeActivity.kt
@@ -2,7 +2,9 @@ package br.com.cwi.nespresso_app.presentation.feature.products.coffeeInfo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.cwi.nespresso_app.R
 import br.com.cwi.nespresso_app.databinding.ActivityDetailedCoffeeBinding
+import br.com.cwi.nespresso_app.domain.entity.DetailedCoffee
 import br.com.cwi.nespresso_app.presentation.extension.toMoneyFormat
 import br.com.cwi.nespresso_app.presentation.extension.visibleOrGone
 import com.bumptech.glide.Glide
@@ -12,7 +14,7 @@ class DetailedCoffeeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailedCoffeeBinding
 
-    val viewModel : DetailedCoffeeViewModel by viewModel()
+    private val viewModel : DetailedCoffeeViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,9 +22,16 @@ class DetailedCoffeeActivity : AppCompatActivity() {
         setContentView(binding.root)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val id = intent.extras?.getInt("id")
+        setup()
+    }
 
-        if(id != null) {
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
+    private fun setup() {
+        intent.extras?.getInt("id")?.let { id ->
             viewModel.fetchDetailedCoffees(id)
 
             viewModel.loading.observe(this@DetailedCoffeeActivity, {
@@ -37,25 +46,24 @@ class DetailedCoffeeActivity : AppCompatActivity() {
                 viewModel.fetchDetailedCoffees(id)
             }
 
-        } else {
-            finish()
+            viewModel.coffees.observe(this, { initViewValues(it) })
+        }
+    }
+
+    private fun initViewValues(it : DetailedCoffee) {
+        val maxIntensity = 20
+        supportActionBar?.title = it.name
+
+        binding.tvDescription.text = it.description
+        binding.tvRoasting.text = it.roasting
+        binding.tvIntensity.text = getString(R.string.txt_intensity, it.intensity)
+        binding.tvPrice.text = it.unitPrice.toMoneyFormat()
+        binding.tvOrigin.text = it.origin
+        binding.tvProfile.text = it.profile
+        it.intensity?.let { intensity ->
+            binding.lpiIntensity.progress = ((intensity.toDouble() / maxIntensity.toDouble()) * 100.0).toInt()
         }
 
-        viewModel.coffees.observe(this, {
-            val maxIntensity = 13
-            supportActionBar?.title = it.name
-
-            binding.tvDescription.text = it.description
-            binding.tvRoasting.text = it.roasting
-            binding.tvIntensity.text = "Intensidade: ${it.intensity.toString()}"
-            binding.tvPrice.text = it.unitPrice.toMoneyFormat()
-            binding.tvOrigin.text = it.origin
-            binding.tvProfile.text = it.profile
-            it.intensity?.let { intensity ->
-                binding.lpiIntensity.progress = ((intensity.toDouble() / maxIntensity.toDouble()) * 100.0).toInt()
-            }
-
-            Glide.with(this).load(it.urlImage).into(binding.ivCoffee)
-        })
+        Glide.with(this).load(it.urlImage).into(binding.ivCoffee)
     }
 }
